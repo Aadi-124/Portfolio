c508f725221cd72f3be2468bafb6c74ca6f739ee
Aaditya Thakare <aadityathakare124@gmail.com>
2024-07-08 21:27:04 +0530

    Responsive Issue Resolved!

--- src/Components/Profile.jsx
+++ src/Components/Profile.jsx
@@ -2,27 +2,27 @@
 
 import profile from '../assets/profilepic.png';
 import './Responsive.css';
-import Aos from 'aos';
-import { useEffect } from 'react';
-import 'aos/dist/aos.css';
+// import Aos from 'aos';
+// import { useEffect } from 'react';
+// import 'aos/dist/aos.css';
 
 export default function Profile() {
 
-    useEffect(()=>{
-        Aos.init({duration:2000});
-    },[]);
+    // useEffect(()=>{
+    //     Aos.init({duration:2000});
+    // },[]);
 
     return (
         <>
             <div className='mainDiv'>
 
                 <div className='profileMainDiv'>
-                    <div className='profilepicDiv'  data-aos="fade-right"><center>
+                    <div className='profilepicDiv' ><center>
                         <img src={profile} alt="profile picture" className='profilepic glowing-circle' />
                         <p className='profileName'><u style={{ "color": "#41bcf0" }}>AADITYA</u> <u style={{ "color": "yellow" }}>THAKARE</u></p></center>
                     </div>
                     
-                    <div className='profileDescription'  data-aos="fade-left">
+                    <div className='profileDescription' >
 
                         <p>Hey! I am Aaditya Thakare A Full Stack JAVA Developer!</p>
                         <div className='btnResume'  data-aos="fade-up">
